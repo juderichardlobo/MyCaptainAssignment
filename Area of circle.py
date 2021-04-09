73117026251ccd538bf84e4d6ae5ff57b8aaa897
juderichardlobo <82036421+juderichardlobo@users.noreply.github.com>
2021-04-09 18:40:53 +0530

    Add files via upload
    First Assignment

---
+++ Area of circle.py
@@ -0,0 +1,6 @@
+import math as M
+#importing the math function 
+r = float(input("Input Radius Of Circle : "))
+#float because decimal point value is needed
+A = M.pi * r * r
+print("Area Of The Circle is  = ",A)
