ef687443f38e94a16c87d4882f2bf61f6ea57ad7
juderichardlobo <82036421+juderichardlobo@users.noreply.github.com>
2021-04-16 18:54:56 +0530

    Add files via upload

---
+++ Positive integers in range.py
@@ -0,0 +1,22 @@
+# declaring list of numbers
+list1 = [12,-7,5,64,-14]
+list2 = [12,14,-95,3]
+print("The output for list 1 is : ")
+  
+# iterating each number in list
+for num in list1:      
+    # sorting the positive integers :
+    if num >= 0:
+       print("list 1:(" ,num,"),")
+
+#repeated for list 2
+print("The output for list 2 is : ")
+for num in list2:
+    if num>= 0:
+        print("list 2:(", num,"),")
+
+
+
+
+
+    
